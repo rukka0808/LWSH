2d644fc33e5da61c255676ba16bc8f4d6d300c5c
rukka7 <rukka7@naver.com>
2021-02-03 01:36:54 +0900

    init

--- app.js
+++ app.js
@@ -26,5 +26,5 @@ app.use((err, req, res, next) => {                  // 에러 처리
 });
 
 app.listen(app.get('port'), () => {
-    console.log(app.get('port'), '번 포트에서 대기 중');
+    console.log(app.get('port'), '  번 포트에서 대기 중');
 });
